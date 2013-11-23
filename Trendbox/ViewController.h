4b3837c5310528c95487de113b58d57f5e3407be
Moritz C. Türck <m.tuerck@clueda.com>
2013-11-23 15:54:26 +0100

    Initial Commit

---
+++ Trendbox/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Trendbox
+//
+//  Created by Moritz C. Türck on 23.11.13.
+//  Copyright (c) 2013 Moritz C. Türck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Trendbox/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Trendbox
+//
+//  Created by Moritz C. Türck on 23.11.13.
+//  Copyright (c) 2013 Moritz C. Türck. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
